9228723209f663a6da0a8bae24fc01c5762700d9
Joshua Russel Uy <116649829+JOSHUA-A69@users.noreply.github.com>
2023-09-05 22:30:36 +0800

    Add files via upload

--- Act#3.c
+++ Act#3.c
@@ -1,48 +1,36 @@
 /*C program that accepts 10 letters.  These letters should be either ‘a’, ‘b’, or ‘c’ only. It should store these into an array. 
  The program counts the number of ‘a’, ‘b’, or ‘c’. Display these counts.*/
- 
- 
  #include <stdio.h>
+ #define letters 10
 
-#define letters 10
-
-void acceptletters(char letters[letters]){
+void acceptletters(char lett[letters]){
 	int ctr;
 	
 	printf("Input 10 letterss: ");
 	for(ctr = 0; ctr < letters; ctr++){
 		printf("\n#%c: ", ctr+1);
-		scanf("%c", &letters[ctr]);
+		scanf("%c", &lett[ctr]);
 	}
 }
 
-void displayNum(int num[size]){
-	int ctr;
+void countletters(char lett[letters]){
+	int ctr, 'a'= 0, 'b' = 0, 'c' = 0;
 	
-	printf("\nContents of the array: ");
-	for(ctr = 0; ctr < size; ctr++){
-		printf("\n#%d: %d", ctr+1, num[ctr]);
-	}
-}
-
-void countEvenOdd(int num[size]){
-	int ctr, odd = 0, even = 0;
-	
-	for(ctr = 0; ctr < size; ctr++){
+	for(ctr = 0; ctr < letters; ctr++){
 		if(num[ctr] % 2 == 0)
-		  even++;
-		else
-		  odd++;  
+		  'a'++;
+		if else
+		  'b'++;  
+	    else
+		'c'++;
 	}
-	displayNum(num);
-	printf("\nOdd = %d", odd);
-	printf("\nEven = %d", even);
+	displaynumberofabc(lett);
+	printf("\nNumber of A = %c", 'a');
+	printf("\nNumber of B = %c", 'b');
+	printf("\nNumber of B = %c", 'c');
 }
 
-main(){
-	char letters[letters];
+int main(){
 	
-	acceptletters(letters);
-	displayNum(num);
-	countEvenOdd(num);
+	
 }
