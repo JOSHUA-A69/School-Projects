1c7501ce718ba4285730857ae73965f661c42852
Joshua Russel Uy <116649829+JOSHUA-A69@users.noreply.github.com>
2023-09-08 22:46:38 +0800

    Add files via upload

---
+++ Act#4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void acceptScores(float scores[3][4]) {
+    printf("Enter scores for 3 students and 4 quizzes:\n");
+    
+    for (int i = 0; i < 3; i++) {
+        printf("Student %d\n", i + 1);
+        
+        for (int j = 0; j < 4; j++) {
+            printf("  Quiz %d: ", j + 1);
+            scanf("%f", &scores[i][j]);
+        }
+    }
+}
+
+void displayScores(float scores[3][4]) {
+    printf("\nStudent scores\n");
+
+    for (int i = 0; i < 3; i++) {
+        printf("Student %d\n", i + 1);
+        
+        for (int j = 0; j < 4; j++) {
+            printf("  Quiz %d %.2f\n", j + 1, scores[i][j]);
+        }
+    }
+}
+
+
+int main(){
+float scores[3][4];
+
+    acceptScores(scores);
+    displayScores(scores);
+}
