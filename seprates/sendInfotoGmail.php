98a2672377f954cc6a36a2b11115f438e5ff0933
Joshua Russel Uy <116649829+JOSHUA-A69@users.noreply.github.com>
2023-09-13 22:42:54 +0800

    Add files via upload

---
+++ seprates/sendInfotoGmail.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $company = $_POST["company"];
+    $email = $_POST["email"];
+    $message = $_POST["comment"];
+    
+    // Compose and send an email
+    $to = "ujoshua368@gmail.com";
+    $subject = "New Contact Form Submission";
+    $messageBody = "First Name: $fname\nLast Name: $lname\nCompany: $company\nEmail: $email\nMessage:\n$message";
+    
+    // Additional headers
+    $headers = "From: $email\r\n";
+    
+    // Send the email
+    if (mail($to, $subject, $messageBody, $headers)) {
+        echo "Your message has been sent successfully.";
+    } else {
+        echo "Sorry, there was an error sending your message.";
+    }
+}
+?>
