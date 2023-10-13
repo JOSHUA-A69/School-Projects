c8e8e401c599f53b5714ec38c5c54263a148b7bf
Joshua Russel Uy <116649829+JOSHUA-A69@users.noreply.github.com>
2023-10-13 09:14:15 +0800

    Add files via upload

---
+++ seprates/transitions.js
@@ -0,0 +1,50 @@
+document.getElementById("goTo1-education").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.classList.add("fade-out");
+    setTimeout(function () {
+      window.location.href = "1.education.html";
+    }, 500);
+  });
+  
+  document.getElementById("goTo2-work-experience").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.classList.add("fade-out");
+    setTimeout(function () {
+      window.location.href = "2.work-experience.html";
+    }, 500);
+  });
+  
+  document.getElementById("goTo3-skills-certificates").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.classList.add("fade-out");
+    setTimeout(function () {
+      window.location.href = "3.skills-certificates.html";
+    }, 500);
+  });
+  
+  document.getElementById("goTo4-clubs").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.classList.add("fade-out");
+    setTimeout(function () {
+      window.location.href = "4.clubs.html";
+    }, 500);
+  });
+  
+  document.getElementById("goTo5-bio").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.classList.add("fade-out");
+    setTimeout(function () {
+      window.location.href = "5.bio.html";
+    }, 500);
+  });
+  
+  document.getElementById("goTo6-hire-me").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.classList.add("fade-out");
+    setTimeout(function () {
+      window.location.href = "6.hire-me.html";
+    }, 500);
+  });
+  
+ 
+  
